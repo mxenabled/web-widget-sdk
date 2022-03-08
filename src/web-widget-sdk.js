2445dac7ef64f54bc61b975e14a6d010e7545741
Sean Brown <sean.brown@mx.com>
2022-03-07 17:36:11 -0700

    adding title option, testing window and document post message listening

--- src/web-widget-sdk.js
+++ src/web-widget-sdk.js
@@ -3,6 +3,7 @@ const requiredOptions = ['id', 'url']
 const defaultValues = {
   height: 400,
   width: 500,
+  title: 'MX Web Widget',
 }
 
 class WebWidgetSDK {
@@ -13,6 +14,7 @@ class WebWidgetSDK {
       this.options = options
 
       this.#validateOptions()
+      this.#setupPostMessages()
       this.#buildIframe()
 
       console.log('no instance yet', this)
@@ -38,11 +40,36 @@ class WebWidgetSDK {
     const iframe = document.createElement('iframe')
 
     iframe.src = this.options.url
-    iframe.height = this.options.height ?? defaultValues.height
+    iframe.title = this.options.title ?? defaultValues.title
     iframe.width = this.options.width ?? defaultValues.width
+    iframe.height = this.options.height ?? defaultValues.height
 
     const node = document.querySelector(this.options.id)
     node.appendChild(iframe)
+  }
+
+  #setupPostMessages() {
+    window.addEventListener(
+      'message',
+      (event) => {
+        console.log('window message received', event)
+        // if (event.origin !== 'http://example.org:8080') return
+
+        // ...
+      },
+      false,
+    )
+
+    document.addEventListener(
+      'message',
+      (event) => {
+        console.log('document message received', event)
+        // if (event.origin !== 'http://example.org:8080') return
+
+        // ...
+      },
+      false,
+    )
   }
 }
 
