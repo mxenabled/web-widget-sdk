91dbe853411fe3dbf1f3c7ece635c87523b27ef4
Sean Brown <sean.brown@mx.com>
2022-03-07 17:27:22 -0700

    adding prettierrc, updating web-widget-sdk with prettier rules

--- src/web-widget-sdk.js
+++ src/web-widget-sdk.js
@@ -6,42 +6,41 @@ const defaultValues = {
 }
 
 class WebWidgetSDK {
-
   constructor(options) {
     if (!WebWidgetSDK._instance) {
-      WebWidgetSDK._instance = this;
-      
-      this.options = options;
+      WebWidgetSDK._instance = this
+
+      this.options = options
 
       this.#validateOptions()
       this.#buildIframe()
 
-      console.log("no instance yet", this)
+      console.log('no instance yet', this)
     }
 
-    console.log("instance already exists", this)
-    return WebWidgetSDK._instance;
+    console.log('instance already exists', this)
+    return WebWidgetSDK._instance
   }
 
   static getInstance() {
-    return this._instance;
+    return this._instance
   }
 
   #validateOptions() {
-    requiredOptions.forEach(option => {
+    requiredOptions.forEach((option) => {
       if (!this.options[option]) {
         throw new Error(`No ${option} provided`)
-        }
+      }
     })
   }
 
   #buildIframe() {
-    const iframe = document.createElement('iframe');
+    const iframe = document.createElement('iframe')
 
-    iframe.src = this.options.url;
+    iframe.src = this.options.url
     iframe.height = this.options.height ?? defaultValues.height
     iframe.width = this.options.width ?? defaultValues.width
-    
+
     const node = document.querySelector(this.options.id)
     node.appendChild(iframe)
   }
