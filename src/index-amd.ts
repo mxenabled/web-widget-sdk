f91786ce98e970af72b0bef8af8d19c341a27497
Marcos Minond <marcos.minond@mx.com>
2022-03-18 18:43:55 -0600

    Noting reasong for file

--- src/index-amd.ts
+++ src/index-amd.ts
@@ -1,3 +1,7 @@
+/**
+ * This file is the entry point for the AMD module built with browserify and
+ * babel.
+ */
 import * as widgets from "./widgets"
 
 declare function define<T>(name: string, mod: () => T)
