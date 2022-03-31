9340bdcc08194c91a99252b1567213ce1829db7a
Marcos Minond <marcos.minond@mx.com>
2022-03-30 21:22:24 -0600

    Post message tests

--- test/integration/widget_test.ts
+++ test/integration/widget_test.ts
@@ -6,4 +6,14 @@ describe("Widget", () => {
   it("loads the widget in an iframe", () => {
     cy.widgetIframe().should("exist")
   })
+
+  describe("Post Message Callbacks", () => {
+    it("mx/load", () => {
+      cy.get("[data-log-id=onLoad]").should("exist")
+    })
+
+    it("mx/connect/loaded", () => {
+      cy.get("[data-log-id=onLoaded]").should("exist")
+    })
+  })
 })
