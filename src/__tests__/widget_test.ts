7e9697ab503d466c19ea94777953de8b2e9f74f4
Marcos Minond <marcos.minond@mx.com>
2022-04-05 12:55:09 -0600

    Adding event listener management unit tests

--- src/__tests__/widget_test.ts
+++ src/__tests__/widget_test.ts
@@ -1,6 +1,7 @@
 import { ConnectWidget } from "../../src"
 
 describe("ConnectWidget", () => {
+  const url = "https://widgets.moneydesktop.com/md/..."
   let widgetContainer = document.createElement("div")
 
   beforeEach(() => {
@@ -17,34 +18,47 @@ describe("ConnectWidget", () => {
   describe("initialization", () => {
     test("an error is thrown when the container element is not found", () => {
       expect(() => {
-        new ConnectWidget({
-          widgetContainer: "#notfound",
-          url: "https://test.com",
-        })
+        new ConnectWidget({ url, widgetContainer: "#notfound" })
       }).toThrow("Unable to find widget container: #notfound")
     })
 
     test("it appends the iframe to the widget container element", () => {
-      new ConnectWidget({
-        widgetContainer,
-        url: "https://test.com",
-      })
+      new ConnectWidget({ url, widgetContainer })
 
       expect(widgetContainer?.children.length).toBe(1)
       expect(widgetContainer?.children[0].tagName).toBe("IFRAME")
+    })
+
+    test("post message event listener is added", () => {
+      const spy = jest.spyOn(window, "addEventListener").mockImplementation(() => ({
+        addEventListener: jest.fn(),
+      }))
+
+      new ConnectWidget({ url, widgetContainer })
+
+      expect(window.addEventListener).toHaveBeenCalledWith("message", expect.anything(), false)
+      spy.mockRestore()
     })
   })
 
   describe("unmount", () => {
     test("it removes the iframe from the container element", () => {
-      const widget = new ConnectWidget({
-        widgetContainer,
-        url: "https://test.com",
-      })
-
+      const widget = new ConnectWidget({ url, widgetContainer })
       widget.unmount()
 
       expect(widgetContainer?.children.length).toBe(0)
+    })
+
+    test("post message event listener is removed", () => {
+      const spy = jest.spyOn(window, "removeEventListener").mockImplementation(() => ({
+        removeEventListener: jest.fn(),
+      }))
+
+      const widget = new ConnectWidget({ url, widgetContainer })
+      widget.unmount()
+
+      expect(window.removeEventListener).toHaveBeenCalledWith("message", expect.anything(), false)
+      spy.mockRestore()
     })
   })
 })
