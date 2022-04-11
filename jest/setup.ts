23a67a2bd58d182800b221ba912c2726305c5558
Marcos Minond <marcos.minond@mx.com>
2022-04-11 17:27:19 -0600

    Updating jest setup documentation

--- jest/setup.ts
+++ jest/setup.ts
@@ -1,11 +1,14 @@
 import fetch from "node-fetch"
 import * as util from "util"
 
-/* The type cast of `global.fetch` to `unknown` is needed because the global
- * `fetch`'s type definition comes from React Native, which is incompatible
- * with the one from the `node-fetch` module. As far as I can tell, there are
- * no run-time differences that impact the behavior we're testing, so using
- * `node-fetch` is a safe alternative to React Native's `fetch`.
+/* The type cast of `global.fetch` and `global.TextDecoder` to `unknown` is
+ * needed to cleanly override the variables.
+ *
+ * fetch is needed because we are running in a Node environment which doesn't
+ * have fetch, and TextDecoder is needed because Jest's jsdom environment
+ * doesn't allow access to it (because it's not part of the DOM API), but
+ * node-fetch requires it, so I'm including it here. Nothing besides fetch uses
+ * TextDecoder.
  */
 // prettier-ignore
 (global.fetch as unknown) = fetch;
