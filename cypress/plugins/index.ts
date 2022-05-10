59dac088a47243c68c613e3ef2d991c23a5669a5
Marcos Minond <marcos.minond@mx.com>
2022-05-10 11:15:51 -0600

    Not running remote asset tests on PR builds

--- cypress/plugins/index.ts
+++ cypress/plugins/index.ts
@@ -1,5 +1,6 @@
 module.exports = (on, config) => {
   config.baseUrl = `http://localhost:${process.env["PORT"] || 8089}`
+  config.skipRemoteAssetTests = process.env["SKIP_REMOTE_ASSET_TESTS"]
 
   return config
 }
